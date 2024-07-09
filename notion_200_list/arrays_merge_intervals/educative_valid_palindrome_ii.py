060ccf8aede4f15972433594d85da4a94e589eed
Sandro Almeida <sandrodotbuild@gmail.com>
2024-07-08 22:42:03 -0700

    coding exercise

--- notion_200_list/arrays_merge_intervals/educative_valid_palindrome_ii.py
+++ notion_200_list/arrays_merge_intervals/educative_valid_palindrome_ii.py
@@ -1,23 +1,16 @@
 def isPalindrome(s):
-    right = 0
-    left = len(s) - 1
-    exclusions = True
+    left = 0
+    right = len(s) - 1
 
-    while right < left:
-        if s[right] == s[left]:
-            right += 1
-            left -= 1
-        elif s[right] == s[left - 1] and exclusions:
-            exclusions = False
-            right += 1
-            left -= 2
-        elif s[right + 1] == s[left] and exclusions:
-            exclusions = False
-            right += 2
-            left -= 1
+    while left < right:
+        if s[left] == s[right]:
+            left += 1
+            right -= 1
         else:
-            return False
+            l_str = s[left+1:right+1]
+            r_str = s[left:right]
+            return l_str == l_str[::-1] or r_str == r_str[::-1]
     return True
 
 
-print(isPalindrome("ABCPBA"))
+print(isPalindrome("ebcbbececabbacecbbcbe"))
