1682bdf7d8ad1f58c135895a476e1fb7fd967b05
Sandro Almeida <sandrodotbuild@gmail.com>
2024-07-11 16:48:41 -0700

    coding exercise

--- notion_200_list/49_group_anagrams.py
+++ notion_200_list/49_group_anagrams.py
@@ -1,19 +1,13 @@
 # my first solution without looking anything
 def groupAnagrams(strs):
-    # build a hash function for a word
-    def getHash(chars):
-        hash = ""
-        for c in chars:
-            hash = hash + str(ord(c))
-        return hash
-    # dict should be hash value as key and list of string matching the hash
+    # dict with sorted value as key and list of string matching the hash
     dict = {}
     for item in strs:
-        hash = getHash(sorted(item))
-        if hash in dict:
-            dict[hash].append(item)
+        sorted_item = "".join(sorted(item))
+        if sorted_item in dict:
+            dict[sorted_item].append(item)
         else:
-            dict[hash] = [item]
+            dict[sorted_item] = [item]
     # iterate the dict to form the final result
     result = []
     for item in dict.values():
