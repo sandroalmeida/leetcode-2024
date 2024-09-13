116085be8534e0074360597de4576030544382c4
Sandro Almeida <sandrodotbuild@gmail.com>
2024-09-13 17:34:15 -0600

    coding exercise

--- notion_200_list/15_3Sum.py
+++ notion_200_list/15_3Sum.py
@@ -26,4 +26,30 @@ def threeSum(nums):
     return result
 
 
-print(threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
+def threeSumWithHash(nums):
+    result = set()
+    for i in range(len(nums)):
+        sub_array = nums[:i] + nums[i+1:]
+        partials = twoSum(sub_array, 0 - nums[i])
+        for v1, v2 in partials:
+            sum_res = [nums[i]] + [v1, v2]
+            sum_res.sort()
+            result.add((sum_res[0], sum_res[1], sum_res[2]))
+    return result
+
+
+def twoSum(sub_nums, target):
+    result = set()
+    dict = {}
+    for idx, value in enumerate(sub_nums):
+        pair = dict.get(value)
+        if pair is not None:
+            if value < pair:
+                result.add((value, pair))
+            else:
+                result.add((pair, value))
+        dict[target - value] = value
+    return result
+
+
+print(threeSumWithHash([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
