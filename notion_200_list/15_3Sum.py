ebe25978b0cda8933654a34d53e6febdc8e8cd2b
Sandro Almeida <sandrodotbuild@gmail.com>
2024-09-14 10:31:07 -0600

    coding exercise

--- notion_200_list/15_3Sum.py
+++ notion_200_list/15_3Sum.py
@@ -52,4 +52,27 @@ def twoSum(sub_nums, target):
     return result
 
 
-print(threeSumWithHash([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
+def threeSumOptimal(nums):
+    result = []
+    nums.sort()
+
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
+
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            sum = a + nums[l] + nums[r]
+            if sum > 0:
+                r -= 1
+            elif sum < 0:
+                l += 1
+            else:
+                result.append([a, nums[l], nums[r]])
+                l += 1
+                while nums[l-1] == nums[l] and l < r:
+                    l += 1
+    return result
+
+
+print(threeSumOptimal([-1,0,1,2,-1,-4]))
